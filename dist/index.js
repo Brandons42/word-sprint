ffab27c31bf4a5c9f707aa8bcf925d0d6548de27
Brandon <brandonbsuen@gmail.com>
2019-10-01 18:56:50 -0700

    Update 2019-10-02T01:56:22.109Z

--- dist/index.js
+++
@@ -1,1 +0,0 @@
-var scrollable=document.getElementById("scrollable");document.getElementById("background").addEventListener("wheel",function(e){scrollable.scrollTop+=e.deltaY});
--- index.js
+++ index.js
@@ -1,5 +1,1 @@
-var scrollable = document.getElementById('scrollable');
-
-document.getElementById('background').addEventListener('wheel', function(e) {
-	scrollable.scrollTop += e.deltaY;
-});
+var scrollable=document.getElementById("scrollable");document.getElementById("background").addEventListener("wheel",function(e){scrollable.scrollTop+=e.deltaY});
