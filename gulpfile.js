f46b4538e40f63cf027a24879c7bb887ff018573
Brandon <brandonbsuen@gmail.com>
2020-04-13 02:09:31 -0700

    Changed description and added videos

--- gulpfile.js
+++ gulpfile.js
@@ -8,26 +8,15 @@ const uglify = require('gulp-uglify');
 const deploy = () => src('./dist/**/*').pipe(ghPages());
 
 const images = () =>
-	src('./images/*.png')
-		.pipe(imagemin())
-		.pipe(dest('./dist/images/'));
+	src('./images/*.png').pipe(imagemin()).pipe(dest('./dist/images/'));
 
-const markup = () =>
-	src('./index.html')
-		.pipe(htmlmin())
-		.pipe(dest('./dist/'));
+const markup = () => src('./index.html').pipe(htmlmin()).pipe(dest('./dist/'));
 
-const scripts = () =>
-	src('./index.js')
-		.pipe(uglify())
-		.pipe(dest('./dist/'));
+const scripts = () => src('./index.js').pipe(uglify()).pipe(dest('./dist/'));
 
-const styles = () =>
-	src('./index.css')
-		.pipe(cleanCSS())
-		.pipe(dest('./dist/'));
+const styles = () => src('./index.css').pipe(cleanCSS()).pipe(dest('./dist/'));
 
-const video = () => src('./endless-mode.mp4').pipe(dest('./dist/'));
+const video = () => src('./videos/*.mp4').pipe(dest('./dist/videos/'));
 
 const build = parallel(images, markup, scripts, styles, video);
 
--- index.js
+++ index.js
@@ -1,5 +1,34 @@
 var scrollable = document.getElementById('scrollable');
 
-document.getElementById('background').addEventListener('wheel', function(e) {
+document.getElementById('background').addEventListener('wheel', function (e) {
 	scrollable.scrollTop += e.deltaY;
 });
+
+var buttons = document.getElementsByClassName('see-video');
+
+var videos = document.getElementsByTagName('video');
+
+var srcs = [
+	'./videos/thesaurus.mp4',
+	'./videos/rhyme.mp4',
+	'./videos/sprint.mp4'
+];
+
+function changeVideo(video, srcVal) {
+	video.pause();
+	video.innerHTML = 'Your browser does not support the video tag.';
+	var source = document.createElement('source');
+	source.setAttribute('src', srcVal);
+	video.appendChild(source);
+	video.load();
+	video.play();
+}
+
+for (var q = 0; q < 3; q++) {
+	buttons[q].onclick = function () {
+		var srcVal = srcs[parseInt(this.id)];
+		console.log(srcVal);
+		changeVideo(videos[0], srcVal);
+		changeVideo(videos[1], srcVal);
+	};
+}
