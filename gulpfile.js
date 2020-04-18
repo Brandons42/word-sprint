fdefef95249fd7d91eb2a7c519ebda1fa9256530
Brandon <brandonbsuen@gmail.com>
2020-04-17 22:01:16 -0700

    SVG Gulp handling added

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ const cleanCSS = require('gulp-clean-css');
 const ghPages = require('gulp-gh-pages');
 const htmlmin = require('gulp-htmlmin');
 const imagemin = require('gulp-imagemin');
+const svgmin = require('gulp-svgmin');
 const uglify = require('gulp-uglify');
 
 const deploy = () => src('./dist/**/*').pipe(ghPages());
@@ -16,9 +17,12 @@ const scripts = () => src('./index.js').pipe(uglify()).pipe(dest('./dist/'));
 
 const styles = () => src('./index.css').pipe(cleanCSS()).pipe(dest('./dist/'));
 
+const svg = () =>
+	src('./images/*.svg').pipe(svgmin()).pipe(dest('./dist/images/'));
+
 const video = () => src('./videos/*.mp4').pipe(dest('./dist/videos/'));
 
-const build = parallel(images, markup, scripts, styles, video);
+const build = parallel(images, markup, scripts, styles, svg, video);
 
 exports.default = build;
 
